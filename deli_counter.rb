a29b7791097b2c7d0b1e3b6c125b107352939214
Monica Seaman <monica.l.seaman@gmail.com>
2018-05-08 13:19:17 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,3 +13,11 @@ def take_a_number(katz_deli, name)
   katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.index(name) + 1} in line."
 end
+
+def now_serving(katz_deli)
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    client_served = katz_deli.shift
+    puts "Currently serving #{client_served}."
+end
