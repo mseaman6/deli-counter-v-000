45fbf4b4717f28fd737bb1556810747984d8d71d
Monica Seaman <monica.l.seaman@gmail.com>
2018-05-08 12:56:55 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,6 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    katz_deli.join("#{katz_deli.index + 1}. ")  
+    katz_deli.join("#{katz_deli.index}. ")  
   end
 end
